5c8480810a7c5e75846f3dff180ef348a9525c5d
arthusagain <arthus.james@hotmail.com>
2020-06-25 20:31:12 -0300

    Tarefa 2 concluida. Estrutura de pastas retrabalhada.

---
+++ Prova-Estagiario/TAREFA2/CsvEditor.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TAREFA2
+{
+    public class CsvLinha : List<string>
+    {
+        public string TextoLinha { get; set; }
+    }
+
+    public class MapaCsv
+    {
+        public string localizacao { get; set; }
+        public string populacao { get; set; }
+    }
+
+
+    public class CsvEditor : StreamReader
+    {
+        public CsvEditor(string path)
+        : base(path)
+        {
+        }
+
+        public bool LerLinha(CsvLinha linha, char separator)
+        {
+            try
+            {
+                linha.TextoLinha = ReadLine();
+                if (String.IsNullOrEmpty(linha.TextoLinha))
+                    return false;
+
+                int pos = 0;
+                int linhaAtual = 0;
+
+                while (pos < linha.TextoLinha.Length)
+                {
+                    string valor;
+
+                    if (linha.TextoLinha[pos] == '"')
+                    {
+                        pos++;
+
+                        int start = pos;
+                        while (pos < linha.TextoLinha.Length)
+                        {
+                            if (linha.TextoLinha[pos] == '"')
+                            {
+                                pos++;
+
+                                if (pos >= linha.TextoLinha.Length || linha.TextoLinha[pos] != '"')
+                                {
+                                    pos--;
+                                    break;
+                                }
+                            }
+                            pos++;
+                        }
+                        valor = linha.TextoLinha.Substring(start, pos - start);
+                        valor = valor.Replace("\"\"", "\"");
+                    }
+                    else
+                    {
+                        int inicio = pos;
+                        while (pos < linha.TextoLinha.Length && linha.TextoLinha[pos] != separator)
+                            pos++;
+                        valor = linha.TextoLinha.Substring(inicio, pos - inicio);
+                    }
+
+                    if (linhaAtual < linha.Count)
+                        linha[linhaAtual] = valor;
+                    else
+                        linha.Add(valor);
+                    linhaAtual++;
+
+                    while (pos < linha.TextoLinha.Length && linha.TextoLinha[pos] != separator)
+                        pos++;
+                    if (pos < linha.TextoLinha.Length)
+                        pos++;
+                }
+                while (linha.Count > linhaAtual)
+                    linha.RemoveAt(linhaAtual);
+
+                return (linha.Count > 0);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ Prova-Estagiario/TAREFA2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TAREFA2
+{
+    class Tarefa2 : CsvEditor
+    {
+        public Tarefa2(string path) : base(path)
+        {
+        }
+
+        public static void Main()
+        {
+
+            using (CsvEditor leitor = new CsvEditor("../../../../../mapa.csv"))
+            {
+                StringBuilder novoArquivo = new StringBuilder();
+                CsvLinha linha = new CsvLinha();
+                char separador = ';';
+
+                leitor.LerLinha(linha, separador);
+                string novaLinha = string.Format("{0}; {1}\n", linha[0], linha[1]);
+                novoArquivo.Append(novaLinha);
+
+                List<MapaCsv> lst = new List<MapaCsv>();
+
+                while (leitor.LerLinha(linha, separador))
+                {
+                    MapaCsv elemento = new MapaCsv();
+                    elemento.localizacao = linha[0];
+                    elemento.populacao = linha[1];
+                    lst.Add(elemento);
+                }
+
+                BubbleSort(lst);
+
+                for (int i = 0; i < lst.Count; i++)
+                {
+                    string localizacao = lst[i].localizacao;
+                    string populacao = lst[i].populacao;
+                    novaLinha = string.Format("{0}; {1}\n", localizacao, populacao);
+                    novoArquivo.Append(novaLinha);
+                }
+
+                File.WriteAllText("../../../resultado.csv", novoArquivo.ToString());
+
+            }
+        }
+
+        public static void BubbleSort(List<MapaCsv> lista)
+        {
+            int tamanho = lista.Count;
+            for (int i = 1; i < tamanho; i++)
+            {
+                for (int j = 0; j < (tamanho - i); j++)
+                {
+                    if (j<tamanho && Int32.Parse(lista[j].populacao) > Int32.Parse(lista[j + 1].populacao))
+                    {
+                        MapaCsv temp = lista[j];
+                        lista[j] = lista[j + 1];
+                        lista[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
